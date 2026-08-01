84da0877102fa8e0fd614fd6dc747a93cce3c41c
Carlos Henrique <heenriqu0103@gmail.com>
2026-08-01 14:40:56 -0300

    Cria verificador de vogais com switch

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// Verificar Vogais
+namespace Aula02_1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("===== VERIFICADOR DE VOGAIS =====");
+            Console.Write("Digite uma letra: ");
+
+            string letra = Console.ReadLine()!.Trim().ToLower();
+
+            switch (letra)
+            {
+                case "a":
+                    Console.WriteLine("A letra A é uma vogal.");
+                    break;
+
+                case "e":
+                    Console.WriteLine("A letra E é uma vogal.");
+                    break;
+
+                case "i":
+                    Console.WriteLine("A letra I é uma vogal.");
+                    break;
+
+                case "o":
+                    Console.WriteLine("A letra O é uma vogal.");
+                    break;
+
+                case "u":
+                    Console.WriteLine("A letra U é uma vogal.");
+                    break;
+
+                default:
+                    Console.WriteLine("A letra digitada não é uma vogal.");
+                    break;
+            }
+        }
+    }
+}
